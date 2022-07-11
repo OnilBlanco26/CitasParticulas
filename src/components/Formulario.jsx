d06c3d507b4f7be29a6f22ceff269c68eb80f5fb
OnilBlanco26 <onilblanco26@gmail.com>
2022-07-11 12:35:46 -0400

    Cabezera Terminanda

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>
-      
+    <div className='md:w-1/2 lg:w-2/5'>
+      <h2 className='font-black text-center text-3xl'>
+        Formulario Registro Pacientes
+      </h2>
+      <p className='text-xl text-center mt-2 mb-5'>
+        Registra tus pacientes y {' '}
+        <span className='font-bold text-orange-400'>
+          Administralos
+        </span>
+      </p>
     </div>
   )
 }
