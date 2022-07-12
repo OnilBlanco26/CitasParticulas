0674e2e5b1005306672f3b23959aec29332d9a6b
OnilBlanco26 <onilblanco26@gmail.com>
2022-07-11 22:47:43 -0400

    Creando el componente state, pasandolo al formulario y luego al ListadoPacientes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import { useState } from "react";
 import Particle from "./components/Particle";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
-import ListadoVehiculos from "./components/ListadoPacientes";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
+
+  const [pacientes, setPacientes] = useState([]);
+
   return (
     <>
      <Particle />
      
         <Header />
         <div className="md:flex">
-        <Formulario />
-        <ListadoVehiculos />
+        <Formulario
+        pacientes = {pacientes}
+        setPacientes = {setPacientes}
+        />
+        <ListadoPacientes
+        pacientes = {pacientes}
+        />
         </div>
         
       
