e1c0f8ce67fc26da36dea319c9a13575235bff3a
OnilBlanco26 <onilblanco26@gmail.com>
2022-07-11 23:15:36 -0400

    APlicando Destructuring para quitar el paciente.

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,40 +1,43 @@
 import React from 'react'
 
 const Pacientes = ({paciente}) => {
+
+  const {nombre, propietario, email, fecha, sintomas} = paciente;
+
   return (
     <div className='bg-orange-400 opacity-80 py-10 px-5 rounded-md mx-2 mb-5 shadow-orange-500 shadow-lg'>
     <p className='font-black uppercase mb-3'>
       Nombre:{' '}
       <span className='font-bold normal-case text-white'>
-          {paciente.nombre}
+          {nombre}
       </span>
     </p>
 
     <p className='font-black uppercase mb-3'>
       Propietario:{' '}
       <span className='font-bold normal-case text-white'>
-          {paciente.propietario}
+          {propietario}
       </span>
     </p>
 
     <p className='font-black uppercase mb-3'>
       Email:{' '}
       <span className='font-bold normal-case text-white'>
-          {paciente.email}
+          {email}
       </span>
     </p>
 
     <p className='font-black uppercase mb-3'>
       Fecha Ingreso:{' '}
       <span className='font-bold normal-case text-white'>
-          {paciente.fecha}
+          {fecha}
       </span>
     </p>
 
     <p className='font-black uppercase mb-3'>
       Sintomas:{' '}
       <span className='font-bold normal-case text-white'>
-          {paciente.sintomas}
+          {sintomas}
       </span>
     </p>
 
