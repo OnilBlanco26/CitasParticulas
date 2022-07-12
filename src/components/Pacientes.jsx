c07e08c3c7503db3e9da951df2d948d44e64ac87
OnilBlanco26 <onilblanco26@gmail.com>
2022-07-11 22:50:44 -0400

    Recibien el arreglo con los pacientes y mostrando la informacion en pantalla

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
 
-const Pacientes = () => {
+const Pacientes = ({paciente}) => {
   return (
     <div className='bg-orange-400 opacity-80 py-10 px-5 rounded-md mx-2 mb-5 shadow-orange-500 shadow-lg'>
     <p className='font-black uppercase mb-3'>
       Nombre:{' '}
       <span className='font-bold normal-case text-white'>
-          Bingo
+          {paciente.nombre}
       </span>
     </p>
 
     <p className='font-black uppercase mb-3'>
       Propietario:{' '}
       <span className='font-bold normal-case text-white'>
-          Onil Blanco
+          {paciente.propietario}
       </span>
     </p>
 
     <p className='font-black uppercase mb-3'>
       Email:{' '}
       <span className='font-bold normal-case text-white'>
-          correo@correo.com
+          {paciente.email}
       </span>
     </p>
 
     <p className='font-black uppercase mb-3'>
       Fecha Ingreso:{' '}
       <span className='font-bold normal-case text-white'>
-          22/10/2022
+          {paciente.fecha}
       </span>
     </p>
 
     <p className='font-black uppercase mb-3'>
       Sintomas:{' '}
       <span className='font-bold normal-case text-white'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quisquam dignissimos in et nemo accusantium omnis cumque deleniti officia ullam tempora perspiciatis repellendus velit vero amet non, cupiditate nesciunt nihil.
+          {paciente.sintomas}
       </span>
     </p>
 
