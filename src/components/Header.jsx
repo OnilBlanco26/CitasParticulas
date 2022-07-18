adb54406f3504e365134138aa8280ad67fb1ef75
Juan Acevedo <jacevedo@Juans-MacBook-Pro.local>
2022-07-17 20:22:45 -0400

    Le cambie el titulo al Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <h1 className='mt-12 mb-10 text-white text-4xl text-center font-black md:w-2/3 mx-auto'>
-        Pet Shop{' '} 
+       Hola Mundo{' '} 
         <span className='text-orange-400'> Calming Night </span>
     </h1>
   )
