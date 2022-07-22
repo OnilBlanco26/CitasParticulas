5a2d48c084f226d2337f09652b5a6719a3a6c4b4
OnilBlanco26 <onilblanco26@gmail.com>
2022-07-22 13:12:02 -0400

    Diseño listado pacientes terminado

--- src/components/ListadoVehiculos.jsx
+++ src/components/ListadoVehiculos.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Pacientes from './Pacientes'
 
 const ListadoVehiculos = () => {
   return (
-    <div className='md:w-1/2 lg:w-3/5'>
+    <div className='md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll'>
       <h2 className='text-center text-white text-3xl font-bold'>
         Listado Pacientes Clinica
       </h2>
@@ -13,39 +14,12 @@ const ListadoVehiculos = () => {
         </span>
       </p>
 
-      <div className='bg-white py-10 px-5 rounded-md shadow-lg shadow-white mr-2'>
-        <p className='font-bold uppercase mb-3'>
-          Nombre:{' '}
-          <span className='font-normal normal-case mb-3'>
-            Thor
-          </span>
-        </p>
-        <p className='font-bold uppercase mb-3'>
-          Propietario:{' '}
-          <span className='font-normal normal-case mb-3'>
-            Fulanito
-          </span>
-        </p>
-        <p className='font-bold uppercase mb-3'>
-          Correo:{' '}
-          <span className='font-normal normal-case mb-3'>
-            correo@correo.com
-          </span>
-        </p>
-        <p className='font-bold uppercase mb-3'>
-          Fecha Ingreso:{' '}
-          <span className='font-normal normal-case mb-3'>
-            2/10/2022
-          </span>
-        </p>
-        <p className='font-bold uppercase'>
-          Sintomas:{' '}
-          <span className='font-normal normal-case mb-3'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-             Cupiditate autem ab quod nulla labore id mollitia sit delectus minus. Repellendus eum perspiciatis perferendis recusandae cumque in odit consequatur voluptas minus.
-          </span>
-        </p>
-      </div>
+      <Pacientes />
+      <Pacientes />
+      <Pacientes />
+      <Pacientes />
+      <Pacientes />
+    
     </div>
   )
 }
