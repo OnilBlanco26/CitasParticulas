3c6899c9832a8c57dc895dc596c8da6f45e3ebba
OnilBlanco26 <onilblanco26@gmail.com>
2022-07-22 13:25:30 -0400

    recibiendo el prop paciente, creando el Destructuring de paciente y pasando las carasteristicas a cada campo correspondiente

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,41 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const Pacientes = () => {
+const Pacientes = ({ paciente }) => {
+  const { mascota, propietario, correo, fecha, sintomas } = paciente;
+
   return (
-    <div className='bg-white py-10 px-5 rounded-md shadow-lg shadow-white mr-2 mb-4'>
-        <p className='font-bold uppercase mb-3'>
-          Nombre:{' '}
-          <span className='font-normal normal-case mb-3'>
-            Thor
-          </span>
-        </p>
-        <p className='font-bold uppercase mb-3'>
-          Propietario:{' '}
-          <span className='font-normal normal-case mb-3'>
-            Fulanito
-          </span>
-        </p>
-        <p className='font-bold uppercase mb-3'>
-          Correo:{' '}
-          <span className='font-normal normal-case mb-3'>
-            correo@correo.com
-          </span>
-        </p>
-        <p className='font-bold uppercase mb-3'>
-          Fecha Ingreso:{' '}
-          <span className='font-normal normal-case mb-3'>
-            2/10/2022
-          </span>
-        </p>
-        <p className='font-bold uppercase'>
-          Sintomas:{' '}
-          <span className='font-normal normal-case mb-3'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-             Cupiditate autem ab quod nulla labore id mollitia sit delectus minus. Repellendus eum perspiciatis perferendis recusandae cumque in odit consequatur voluptas minus.
-          </span>
-        </p>
-      </div>
-  )
-}
+    <div className="bg-white py-10 px-5 rounded-md shadow-lg shadow-white mr-2 mb-4">
+      <p className="font-bold uppercase mb-3">
+        Nombre: <span className="font-normal normal-case mb-3">{mascota}</span>
+      </p>
+      <p className="font-bold uppercase mb-3">
+        Propietario:{" "}
+        <span className="font-normal normal-case mb-3">{propietario}</span>
+      </p>
+      <p className="font-bold uppercase mb-3">
+        Correo: <span className="font-normal normal-case mb-3">{correo}</span>
+      </p>
+      <p className="font-bold uppercase mb-3">
+        Fecha Ingreso:{" "}
+        <span className="font-normal normal-case mb-3">{fecha}</span>
+      </p>
+      <p className="font-bold uppercase">
+        Sintomas:{" "}
+        <span className="font-normal normal-case mb-3">{sintomas}</span>
+      </p>
+    </div>
+  );
+};
 
-export default Pacientes
+export default Pacientes;
