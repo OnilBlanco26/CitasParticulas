a9215afbc3cfaa9c17f202de84b8bdcc119d50f4
OnilBlanco26 <onilblanco26@gmail.com>
2022-07-22 13:44:30 -0400

    Cambiando el texto del encabezado dependiendo de si existen pacientes o no

--- src/components/ListadoVehiculos.jsx
+++ src/components/ListadoVehiculos.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import Pacientes from './Pacientes'
+import React from "react";
+import Pacientes from "./Pacientes";
 
-const ListadoVehiculos = ({pacientes, setPaciente}) => {
+const ListadoVehiculos = ({ pacientes, setPaciente }) => {
   return (
-    <div className='md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll'>
-      <h2 className='text-center text-white text-3xl font-bold'>
-        Listado Pacientes Clinica
-      </h2>
-      <p className='text-xl text-white text-center mt-2 mb-5'>
-        Aqui se muestran los pacientes registrados en la{' '}
-        <span className='text-indigo-600 font-bold'>
-          Clinica
-        </span>
-      </p>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="text-center text-white text-3xl font-bold">
+            Listado Pacientes Clinica
+          </h2>
+          <p className="text-xl text-white text-center mt-2 mb-5">
+            Aqui se muestran los pacientes registrados en la{" "}
+            <span className="text-indigo-600 font-bold">Clinica</span>
+          </p>
 
-      {pacientes.map(paciente => {
-        return (
-          <Pacientes
-            key={paciente.id}  
-          paciente = {paciente}
-          setPaciente = {setPaciente}
-          />
-        )
-      })}
-    
+          {pacientes.map((paciente) => {
+            return (
+              <Pacientes
+                key={paciente.id}
+                paciente={paciente}
+                setPaciente={setPaciente}
+              />
+            );
+          })}
+        </>
+      ) : (
+        <>
+          <h2 className="text-center text-white text-3xl font-bold">
+            No hay pacientes Agregados
+          </h2>
+          <p className="text-xl text-white text-center mt-2 mb-5">
+            Agrega nuevos paciente y se mostraran{" "}
+            <span className="text-indigo-600 font-bold">Aqui</span>
+          </p>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ListadoVehiculos
+export default ListadoVehiculos;
