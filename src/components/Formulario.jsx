6e37cf560ed8a9b589d8cd8383d110a6ec20cdf3
OnilBlanco26 <onilblanco26@gmail.com>
2022-07-22 13:19:08 -0400

    Creando los state

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,115 +1,127 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
+import Error from "./Error";
 
 const Formulario = () => {
+  const [mascota, setMascota] = useState("");
+  const [propietario, setPropietario] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [sintomas, setSintomas] = useState("");
 
-  const [mascota, setMascota] = useState('');
-  const [propietario, setPropietario] = useState('');
-  const [correo, setCorreo] = useState('');
-  const [fecha, setFecha] = useState('');
-  const [sintomas, setSintomas] = useState('');
-
-  const [error , setError] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if([mascota, propietario, correo, fecha, sintomas].includes('')){
+    if ([mascota, propietario, correo, fecha, sintomas].includes("")) {
       setError(true);
       return;
     }
 
     setError(false);
 
-
-
     //Limpiar el formulario
-    setMascota('');
-    setPropietario('');
-    setCorreo('');
-    setFecha('');
-    setSintomas('');
-  }
+    setMascota("");
+    setPropietario("");
+    setCorreo("");
+    setFecha("");
+    setSintomas("");
+  };
 
   return (
-    <div className='md:w-1/2 lg:w-2/5'>
-      <h2 className='text-white text-3xl text-center font-bold'>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="text-white text-3xl text-center font-bold">
         Formulario Registro Pacientes
       </h2>
-      <p className='text-xl text-white text-center mt-2 mb-5'>
-        Registra tus pacientes y {' '}
-        <span className='text-indigo-600 font-bold'>
-          Administralos
-        </span>
+      <p className="text-xl text-white text-center mt-2 mb-5">
+        Registra tus pacientes y{" "}
+        <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form action="" className='bg-white py-10 px-5 rounded-md mx-2 shadow-lg shadow-white mb-5'>
-        <div className='mb-5'>
-          <label htmlFor="mascota" className='block font-bold text-indigo-600'>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white py-10 px-5 rounded-md mx-2 shadow-lg shadow-white mb-5"
+      >
+        {error && (
+          <Error>
+            <p>Todos los campos son obligatorios</p>
+          </Error>
+        )}
+        <div className="mb-5">
+          <label htmlFor="mascota" className="block font-bold text-indigo-600">
             Nombre Mascota:
           </label>
-          <input type="text"
-          id='mascota'
-          placeholder='Escribe el nombre de la mascota'
-          className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
-          value={mascota}
-          onChange={(e) => setMascota(e.target.value)}
+          <input
+            type="text"
+            id="mascota"
+            placeholder="Escribe el nombre de la mascota"
+            className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
+            value={mascota}
+            onChange={(e) => setMascota(e.target.value)}
           />
         </div>
-        <div className='mb-5'>
-          <label htmlFor="propietario" className='block font-bold text-indigo-600'>
+        <div className="mb-5">
+          <label
+            htmlFor="propietario"
+            className="block font-bold text-indigo-600"
+          >
             Nombre Propietario:
           </label>
-          <input type="text"
-          id='propietario'
-          placeholder='Escribe el nombre del propietario'
-          className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
-          value={propietario}
-          onChange={(e) => setPropietario(e.target.value)}
-         />
+          <input
+            type="text"
+            id="propietario"
+            placeholder="Escribe el nombre del propietario"
+            className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
+          />
         </div>
-        <div className='mb-5'>
-          <label htmlFor="email" className='block font-bold text-indigo-600'>
+        <div className="mb-5">
+          <label htmlFor="email" className="block font-bold text-indigo-600">
             Email:
           </label>
-          <input type="email"
-          id='email'
-          placeholder='Contacto del propietario'
-          className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
-          value={correo}
-          onChange={(e) => setCorreo(e.target.value)}
+          <input
+            type="email"
+            id="email"
+            placeholder="Contacto del propietario"
+            className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
+            value={correo}
+            onChange={(e) => setCorreo(e.target.value)}
           />
         </div>
-        <div className='mb-5'>
-          <label htmlFor="fecha" className='block font-bold text-indigo-600'>
+        <div className="mb-5">
+          <label htmlFor="fecha" className="block font-bold text-indigo-600">
             Fecha Ingreso
           </label>
-          <input type="date"
-          id='fecha'
-          className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
-          value={fecha}
-          onChange={(e) => setFecha(e.target.value)}
+          <input
+            type="date"
+            id="fecha"
+            className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div>
-        <div className='mb-5'>
-          <label htmlFor="sintomas" className='block font-bold text-indigo-600'>
+        <div className="mb-5">
+          <label htmlFor="sintomas" className="block font-bold text-indigo-600">
             Sintomas
           </label>
-          <textarea type="text"
-          id='sintomas'
-          placeholder='Especifique los sintomas del paciente'
-          className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
-          value={sintomas}
-          onChange={(e) => setSintomas(e.target.value)}
+          <textarea
+            type="text"
+            id="sintomas"
+            placeholder="Especifique los sintomas del paciente"
+            className="w-full border-gray-500 border-2 p-2 rounded-md font-mono"
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
           />
         </div>
 
-
-        <input type="submit"
-        value='Registrar Paciente'
-        className='w-full bg-indigo-600 p-2 text-white uppercase rounded-md font-bold mt-5 shadow-md shadow-indigo-400 hover:bg-indigo-800 transition-all duration-500 cursor-pointer'
+        <input
+          type="submit"
+          value="Registrar Paciente"
+          className="w-full bg-indigo-600 p-2 text-white uppercase rounded-md font-bold mt-5 shadow-md shadow-indigo-400 hover:bg-indigo-800 transition-all duration-500 cursor-pointer"
         />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
